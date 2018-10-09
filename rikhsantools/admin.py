b15b0e3a132e50f89ab8071848fe62390863f85a
rikhsan <rakhmad.ikhsanudin@gmail.com>
2018-10-10 05:22:50 +0700

    asd

--- rikhsantools/admin.py
+++ rikhsantools/admin.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from sertifikasiguru.models import Photo
+from rikhsantools.models import Photo
 # Register your models here.
 admin.site.register(Photo)
